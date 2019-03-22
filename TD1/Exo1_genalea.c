581758c15ec4efb5319693057497bced2cf55c4d
Utilisateur <user@debian.arcahe.ovh>
2019-03-22 14:52:18 +0100

    d

--- TD1/Exo1_genalea.c
+++ TD1/Exo1_genalea.c
@@ -4,13 +4,33 @@
 #include <unistd.h>
 #include <time.h>
 
+int bernoulli(double p) {
+	double r = (double)random()/RAND_MAX;
+	if (r < p) {
+		return 1;
+	}
+	return 0;
+}
+
+int nb_arrivees (double p0, double p2) {
+	double p1 = 1 - p0 - p2;
+	double r = (double)random()/RAND_MAX;
+	
+	if(r < p0) {
+		return 0; }
+		else { if (r < p0 + p1) { return p1;}
+			else { return 2; }
+			}
+		
+}
 
 int main() {
 	
 	srandom(getpid() + time(NULL));
 	
 	for(int i = 0; i < 10; i++){
-		long int a = random();
+		long int a = random()%10;
 		printf("%ld\n",a);
 	}
+	
 }
